c36cfea41a6c3d481323e7c9826dcdb5193eeeda
ChrisTimperley <christimperley@googlemail.com>
2019-03-28 11:40:41 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+PACKAGE_NAME = 'roshammer'
+VERSION = '0.0.1'
+
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    python_requires='>=3.5',
+    description='A general purpose fuzzer for the Robot Operating System',
+    long_description=open('README.rst').read(),
+    author='Chris Timperley',
+    author_email='christimperley@googlemail.com',
+    url='https://github.com/ChrisTimperley/roshammer',
+    license='Apache License 2.0',
+    install_requires=[
+        'fluffycow>=0.0.4'
+    ],
+    packages=['roshammer'],
+    keywords=['ros', 'fuzzing', 'docker'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
