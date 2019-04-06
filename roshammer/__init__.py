556552458d2cdadda373deb971e597ab57119e22
Chris Timperley <christimperley@googlemail.com>
2019-04-05 21:47:02 -0400

    Added FuzzTarget data structure (#7)
    * added FuzzTarget data structure
    
    * allow multiple nodes, in theory
    
    * added validators
    
    * added basic test
    
    * added validator tests
    
    * added launch_filepath attribute

--- roshammer/__init__.py
+++ roshammer/__init__.py
@@ -2,3 +2,45 @@
 """
 roshammer is a fuzzing and random input generation tool for ROS applications.
 """
+__all__ = ('FuzzTarget',)
+
+from typing import Tuple
+
+import attr
+
+
+@attr.s(frozen=True)
+class FuzzTarget:
+    """Provides a description of the ROS application under test.
+
+    Attributes
+    ----------
+        image: str
+            The name of the Docker image for the ROS application.
+        launch_filepath: str
+            The filepath within the container to the launch file that should
+            be used to launch the application under test.
+        nodes: Tuple[str, ...]
+            The names of the nodes that should be fuzzed.
+        topics: Tuple[str, ...]
+            The names of the topics that should be fuzzed.
+
+    Raises
+    ------
+        ValueError: if no nodes are specified.
+        ValueError: if no topics are specified.
+    """
+    image: str = attr.ib()
+    launch_filepath: str = attr.ib()
+    nodes: Tuple[str, ...] = attr.ib(converter=tuple)
+    topics: Tuple[str, ...] = attr.ib(converter=tuple)
+
+    @nodes.validator
+    def has_at_least_one_node(self, attribute, nodes) -> bool:
+        if not nodes:
+            raise ValueError('at least one node must be specified.')
+
+    @topics.validator
+    def has_at_least_one_topic(self, attribute, topics) -> bool:
+        if not topics:
+            raise ValueError('at least one topic must be specified.')
