e6113b25e97cfc88d414fca89298127986766c64
ChrisTimperley <christimperley@googlemail.com>
2019-04-12 17:27:57 -0400

    outlined more of fuzzer

--- roshammer/fuzzer.py
+++ roshammer/fuzzer.py
@@ -1,29 +1,50 @@
 # -*- coding: utf-8 -*-
 __all__ = ('Fuzzer',)
 
-from typing import TypeVar
+from typing import TypeVar, Generic
+import logging
 
 import attr
-from roswire import SystemDescription
+from roswire import SystemDescription, ROSWire
+
+from .input import InputGenerator
 
 T = TypeVar('T')
 
+logger: logging.Logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 
 @attr.s(slots=True)
-class Fuzzer:
+class Fuzzer(Generic[T]):
     """Fuzzes a ROS application according to a given fuzzing strategy.
 
     Attributes
     ----------
+    roswire: ROSWire
+        The ROSWire session for interacting with app instances.
+    image: str
+        The name of the Docker image that provides the ROS application.
     system: SystemDescription
         A description of the system under test.
     inputs: InputGenerator
         Generates inputs that are used to fuzz the system under test.
     """
+    roswire: ROSWire
+    image: str
     system: SystemDescription
     inputs: InputGenerator[T]
+    # need an oracle for assessing inputs: InputChecker
     # we need a way of executing those inputs
     # termination conditions?
 
+    def execute(self, inp: T) -> bool:
+        with self.roswire.launch(self.image, self.system) as sut:
+            pass
+
     def fuzz(self):
-        return
+        for inp in inputs:
+            if self.execute(inp):
+                logger.info("found a crash! %s", inp)
+
+    run = fuzz
