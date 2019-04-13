d98d430f0a14ebaaadaf22db71ea89f7271f2879
Chris Timperley <christimperley@googlemail.com>
2019-04-13 15:35:46 -0400

    Added bag topic filtering (fixes #16) (#29)
    * added mavros.formats.yml
    
    * updated load method to accept a list of topics
    
    * implemented restrict_to_topic
    
    * bug fix: add missing imports
    
    * updated Bag constructor

--- roshammer/bag.py
+++ roshammer/bag.py
@@ -4,7 +4,7 @@ This module provides functionality for fuzzing ROS bags.
 """
 __all__ = ('Bag',)
 
-from typing import Sequence, Iterator, Any
+from typing import Sequence, Iterator, Any, Optional, List, Iterable
 
 from roswire.definitions import TypeDatabase
 from roswire.bag.core import BagMessage
@@ -17,15 +17,29 @@ class Bag(Sequence[BagMessage]):
     """Stores the contents of a ROS bag."""
     __slots__ = ('__contents', '__length')
 
-    def __init__(self, contents: Sequence[BagMessage]) -> None:
+    def __init__(self, contents: Iterable[BagMessage]) -> None:
         self.__contents = tuple(contents)
         self.__length = len(self.__contents)
 
     @classmethod
-    def load(cls, db_type: TypeDatabase, fn: str) -> 'Bag':
-        """Loads a bag from a given file."""
+    def load(cls,
+             db_type: TypeDatabase,
+             fn: str,
+             topics: Optional[List[str]] = None
+             ) -> 'Bag':
+        """Loads a bag from a given file.
+
+        Arguments
+        ---------
+        db_type: TypeDatabase
+            The type database for the system under test.
+        fn: str
+            The path to the bag file.
+        topics: Optional[List[str]]
+            An optional list of topics to which the bag should be restricted.
+        """
         reader = BagReader(fn, db_type)
-        return Bag(reader)
+        return Bag(reader.read_messages(topics))
 
     def save(self, fn: str) -> None:
         """Saves the contents of the bag to a given file on disk."""
@@ -45,6 +59,10 @@ class Bag(Sequence[BagMessage]):
     def __iter__(self) -> Iterator[BagMessage]:
         """Returns an iterator over the contents of the bag."""
         yield from self.__contents
+
+    def restrict_to_topic(self, topic: str) -> Bag:
+        """Returns a variant of this bag that only represents a given topic."""
+        return Bag(m for m in self.__contents if m.topic == topic)
 
 
 class BagInput(Input[Bag]):
