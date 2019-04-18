cfd05d7a4a1a00b2cbe4f10ddcdbcf907a041956
Chris Timperley <christimperley@googlemail.com>
2019-04-18 16:45:18 -0400

    Added basic random input generator (fixes #38) (#42)
    * added search module
    
    * bug fix: missing import
    
    * implemented __next__
    
    * bug fix: missing import
    
    * bug fix: use random.sample

---
+++ roshammer/search.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+This module implements a number of search-based fuzzing strategies.
+"""
+from typing import TypeVar, FrozenSet
+import random
+
+import attr
+
+from .core import Input, InputGenerator, Mutator
+
+T = TypeVar('T')
+
+
+@attr.s
+class RandomInputGenerator(InputGenerator[T]):
+    """
+    Generates a stream of random inputs using a pool of seed inputs and an
+    input mutator.
+    """
+    seeds: FrozenSet[T] = attr.ib(converter=frozenset)
+    mutator: Mutator[T] = attr.ib()
+
+    def __next__(self) -> Input[T]:
+        seed: T = random.sample(self.seeds, 1)[0]
+        inp: Input[T] = Input(seed)
+        return self.mutator(inp)
